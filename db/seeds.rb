77e35b279c7ce6137f38c68a8fb07cd954801788
github username <github email address>
2020-02-11 17:13:31 -0600

    creation

---
+++ db/seeds.rb
@@ -0,0 +1,52 @@
+# users_list = [
+#     'Alejandro', 'Tobias', 'Lindsay', 'GOB', 'Oscar'
+# ]
+
+# users_list.each do |user, index| 
+#     User.create( username: user, password: 'guest')
+# end
+
+# 5.times do
+#     Note.create(
+#         user_id: 1,
+#         title: Faker::TvShows::RickAndMorty.location,
+#         content: Faker::TvShows::RickAndMorty.quote,
+#         tags: 'Tag1, Tag2, Tag3'
+#     )
+# end
+
+# 5.times do
+#     Note.create(
+#         user_id: 2,
+#         title: Faker::TvShows::RickAndMorty.location,
+#         content: Faker::TvShows::RickAndMorty.quote,
+#         tags: 'Tag1, Tag2, Tag3'
+#     )
+# end
+
+# 5.times do
+#     Note.create(
+#         user_id: 3,
+#         title: Faker::TvShows::RickAndMorty.location,
+#         content: Faker::TvShows::RickAndMorty.quote,
+#         tags: 'Tag1, Tag2, Tag3'
+#     )
+# end
+
+# 5.times do
+#     Note.create(
+#         user_id: 4,
+#         title: Faker::TvShows::RickAndMorty.location,
+#         content: Faker::TvShows::RickAndMorty.quote,
+#         tags: 'Tag1, Tag2, Tag3'
+#     )
+# end
+
+# 5.times do
+#     Note.create(
+#         user_id: 5,
+#         title: Faker::TvShows::RickAndMorty.location,
+#         content: Faker::TvShows::RickAndMorty.quote,
+#         tags: 'Tag1, Tag2, Tag3'
+#     )
+# end
