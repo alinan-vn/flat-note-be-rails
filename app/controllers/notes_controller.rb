8856960b26d06ff4897ecf4bd0def4b462c44659
alinan-vn <jandrolinan@gmail.com>
2020-02-15 22:25:13 -0600

    Can create new notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -13,6 +13,16 @@ class NotesController < ApplicationController
     end
 
     def create
+        # note = Note.create(strong_params(params[:user_id] so on))
+
+        note = Note.create(
+            user_id: 1, 
+            title: params[:title],
+            content: params[:content],
+            tags: params[:tags]
+        )
+
+        render json: JSON.pretty_generate(NoteSerializer.new(note).serializable_hash)
     end
 
     def edit
@@ -27,6 +37,14 @@ class NotesController < ApplicationController
         params.require(:note).permit(*args)
     end
 end
+
+# def create
+#     artist = Artist.create(name: params['artistName']['name'])
+#     genre = Genre.create(name: params['genreName']['name'])
+#     connection = Connection.create(artist_id: artist.id, genre_id: genre.id)
+    
+#     render json: connection, include: [:artist, :genre]
+# end
 
 # include strong params
 # return feedback for CRUD functionalities if oject isn't found or other types of errors*****
